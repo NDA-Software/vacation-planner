95ee5f86de333a805911a3859fb188841612f5b7
Herr Turing <admin@nda.software>
2024-03-13 13:33:49 -0300

    Added printing.

--- src/next/pages/index.tsx
+++ src/next/pages/index.tsx
@@ -1,7 +1,9 @@
 import { readFileSync, existsSync, writeFileSync } from 'fs';
 
-import { type ReactElement, useEffect, useState } from 'react';
+import { type ReactElement, useEffect, useState, useRef } from 'react';
 import { CssVarsProvider, Sheet, CssBaseline, Typography, Stack, Table, Button, Tooltip } from '@mui/joy';
+
+import { useReactToPrint } from 'react-to-print';
 
 import Client, { type messageReceiver } from 'socket-actions/client';
 
@@ -30,8 +32,11 @@ export default function Home({ startingData }: propType): ReactElement<any, any>
     const { formActive: formActiveQuery } = router.query;
 
     const [formActive, setFormActive] = useState<boolean>(formActiveQuery === '1');
-
     const [selectedItem, setSelectedItem] = useState<vacationType | undefined>();
+
+    const [printing, setPrinting] = useState<boolean>(false);
+
+    const printRef = useRef<HTMLTableElement>(null);
 
     const connect = (onMessage: messageReceiver): void => {
         setClient(new Client({
@@ -74,6 +79,19 @@ export default function Home({ startingData }: propType): ReactElement<any, any>
         client?.sendAction('removePlan', { key });
     };
 
+    const print = useReactToPrint({
+        content: () => printRef.current,
+        pageStyle: () => 'margin: 50px',
+        onAfterPrint: () => { setPrinting(false); }
+    });
+
+    const clickPrint = (): void => {
+        setPrinting(true);
+
+        setTimeout(print, 1);
+        setTimeout(print, 1);
+    };
+
     useEffect(() => {
         connect(async (message) => {
             const { newData } = JSON.parse(message.data);
@@ -83,7 +101,7 @@ export default function Home({ startingData }: propType): ReactElement<any, any>
     }, []);
 
     return (
-        <CssVarsProvider defaultMode="dark">
+        <CssVarsProvider defaultMode={printing ? 'light' : 'dark'}>
             <CssBaseline />
 
             <Sheet sx={{ padding: '8px', height: '100%', width: '100%', boxSizing: 'border-box' }}>
@@ -99,57 +117,67 @@ export default function Home({ startingData }: propType): ReactElement<any, any>
                     </Stack>
                     : formActive
                         ? <Form client={client} clickClose={clickClose} selectedItem={selectedItem} />
-                        : <Table stickyHeader hoverRow sx={{ height: '100%' }}>
-                            <thead>
-                                <tr>
-                                    <th>Title</th>
-                                    <th>Description</th>
-                                    <th>Date</th>
-                                    <th>Location</th>
-                                    <th>Participants</th>
-                                    <th style={{ width: '16%' }}>Actions <Button variant="plain" onClick={clickNew}>+</Button></th>
-                                </tr>
-                            </thead>
+                        : <>
+                            <Typography level="h3" textAlign="center">
+                                    Vacations
+                            </Typography>
 
-                            <tbody>
-                                {data.map((item, key) => {
-                                    const { title, description, date, location, participants } = item;
+                            <Button variant="plain" onClick={clickNew} sx={{ position: 'absolute', top: '8px', right: '70px' }}>New</Button>
 
-                                    const dateString = new Date(date).toLocaleDateString();
-                                    const participatnsString = (participants ?? []).join(', ');
+                            <Button variant="plain" onClick={clickPrint} sx={{ position: 'absolute', top: '8px', right: '8px' }}>Print</Button>
 
-                                    return <tr key={`vacation-${key}`}>
-                                        <Tooltip title={title}>
-                                            <th>{title}</th>
-                                        </Tooltip>
+                            <Table stickyHeader hoverRow sx={{ height: '100%' }} ref={printRef} >
+                                <thead>
+                                    <tr>
+                                        <th>Title</th>
+                                        <th>Description</th>
+                                        <th>Date</th>
+                                        <th>Location</th>
+                                        <th>Participants</th>
+                                        <th style={!printing ? { width: '16%' } : { display: 'none' }}>Actions</th>
+                                    </tr>
+                                </thead>
 
-                                        <Tooltip title={description}>
-                                            <th>{description}</th>
-                                        </Tooltip>
+                                <tbody>
+                                    {data.map((item, key) => {
+                                        const { title, description, date, location, participants } = item;
 
-                                        <Tooltip title={dateString}>
-                                            <th>{dateString}</th>
-                                        </Tooltip>
+                                        const dateString = new Date(date).toLocaleDateString();
+                                        const participatnsString = (participants ?? []).join(', ');
 
-                                        <Tooltip title={location}>
-                                            <th>{location}</th>
-                                        </Tooltip>
+                                        return <tr key={`vacation-${key}`}>
+                                            <Tooltip title={title}>
+                                                <th>{title}</th>
+                                            </Tooltip>
 
-                                        <Tooltip title={participatnsString}>
-                                            <th>{participatnsString}</th>
-                                        </Tooltip>
+                                            <Tooltip title={description}>
+                                                <th>{description}</th>
+                                            </Tooltip>
 
-                                        <th>
-                                            <Stack direction="row" gap={1}>
-                                                <Button variant='plain' onClick={() => { clickEdit(item, key); }}>+</Button>
+                                            <Tooltip title={dateString}>
+                                                <th>{dateString}</th>
+                                            </Tooltip>
 
-                                                <Button variant='plain' color="danger" onClick={() => { clickRemove(key); }}>X</Button>
-                                            </Stack>
-                                        </th>
-                                    </tr>;
-                                })}
-                            </tbody>
-                        </Table>
+                                            <Tooltip title={location}>
+                                                <th>{location}</th>
+                                            </Tooltip>
+
+                                            <Tooltip title={participatnsString}>
+                                                <th>{participatnsString}</th>
+                                            </Tooltip>
+
+                                            <th style={{ display: (printing ? 'none' : 'block') }}>
+                                                <Stack direction="row" gap={1}>
+                                                    <Button variant='plain' onClick={() => { clickEdit(item, key); }}>+</Button>
+
+                                                    <Button variant='plain' color="danger" onClick={() => { clickRemove(key); }}>X</Button>
+                                                </Stack>
+                                            </th>
+                                        </tr>;
+                                    })}
+                                </tbody>
+                            </Table>
+                        </>
                 }
             </Sheet>
         </CssVarsProvider>
